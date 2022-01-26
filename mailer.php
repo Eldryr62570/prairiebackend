209f80cebe4d8ccc7ca3512ced7c19ccd725e834
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-01-26 14:02:59 +0100

    bddv1

--- mailer.php
+++ mailer.php
@@ -1,38 +1,5 @@
-<?php 
-    //Insertion en bdd des tickets envoyés au support
-   if(isset($_POST)){
-    session_start();
-    $to = 'jordan.moulin62570@gmail.com';
-    $subject= $_POST["subject"];
-    $tel = $_POST["tel"];
-    $age = $_POST["age"];
-    $message = $_POST["message"];
-    try{
-        $db = new PDO(
-            'mysql:host=localhost;dbname=spawnarena;charset=utf8',
-            'root',//Username de la BDD
-            ''     //Password de la BDD
-            
-        );
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(Exeption $e){
-        die('Erreur : ' . $e->getMessage());
-    }
-    //Requête
-    $query = 'INSERT INTO email_send(subject, tel, age, message) VALUES (:subject, :tel, :age, :message)';
-    //Preparation de la Requête
-    $insertMail = $db ->prepare($query);
-    //Insertion en Base
-    $insertMail -> execute([
-        'subject' => $_POST["subject"],
-        'tel'=> $_POST["tel"],
-        'age' => $_POST["age"],
-        'message' => $_POST["message"],
-    ]);
-
-    //Envoie d'un mail 
-    if(isset($_POST)){
+<?php
+    
     $message = '<h1> Problème du client :'.$_POST["message"].'</h1>';
     $headers[]= 'MIME-Version: 1.0'; 
     $headers[]= 'Content-type: text/html; charset=iso-8859-1';
@@ -42,8 +9,6 @@
     if(isset($_SESSION)){
         $_SESSION["mysession"] = true;
     }
-}
         
-}
-    header("Location:presentation.php");
-?>
+        header("Location:presentation.php");
+        
