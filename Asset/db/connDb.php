6e44d1252f8209857c7cf4734527b68c6127de2f
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-01-26 14:28:22 +0100

    Début de create DB

--- Asset/db/connDb.php
+++ Asset/db/connDb.php
@@ -6,6 +6,7 @@ $psw = '';
 $port = 3306;
 $charset ='utf8mb4';
 $dsn ="mysql:host=$host;dbname=$db;port=$port;charset=$charset";
+//Options de debug de la BDD
 $options = [
     \PDO::ATTR_ERRMODE              => \PDO::ERRMODE_EXCEPTION,
     \PDO::ATTR_DEFAULT_FETCH_MODE   => \PDO::FETCH_ASSOC,
@@ -14,12 +15,13 @@ $options = [
 ];
 
 try{
-    $db = new \PDO($dsn,$user,$options[]);
-    echo 'Database connexion established';
+     echo 'Database connexion established';
 
 }
+//Gestion des exeptions
 catch(\PDOException $e){
     throw new \PDOException ($e -> getmessage(),$e -> getcode());
 }
+
 
 
